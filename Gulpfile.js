6f368d81bc4957c599e32ed2c7ae9fdc192c1bd1
Matt Pace <matthew.dp.96@gmail.com>
2020-04-23 23:27:52 -0700

    Add browsersync and grunt watch command

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,13 @@
 const gulp = require("gulp");
 const pug = require("gulp-pug");
+const browserSync = require("browser-sync").create();
 
-const config = require('./config.json');
+const config = require("./config.json");
 
 const paths = {
   styles: {
     src: ["src/css/**/*.css"],
-    dest: "build/css"
+    dest: "build/css",
   },
   templates: {
     src: ["src/templates/**/*.pug", "!src/templates/layouts/**/*.pug"],
@@ -18,13 +19,36 @@ function templates() {
   return gulp
     .src(paths.templates.src)
     .pipe(pug({ locals: config.locals }))
-    .pipe(gulp.dest(paths.templates.dest));
+    .pipe(gulp.dest(paths.templates.dest))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      })
+    );
 }
 
 function styles() {
   return gulp
     .src(paths.styles.src)
-    .pipe(gulp.dest(paths.styles.dest));
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      })
+    );
 }
 
-exports.default = gulp.parallel(templates, styles);
+gulp.task("build", gulp.parallel(templates, styles));
+
+gulp.task("watch", () => {
+  browserSync.init({
+    server: {
+      baseDir: "build",
+    },
+  });
+
+  gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.templates.src, templates);
+});
+
+gulp.task("default", gulp.series("build"));
