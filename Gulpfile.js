75e720afe95d205d5e52ec7d61272ade1d3360fc
Matt Pace <matthew.dp.96@gmail.com>
2020-04-23 22:45:02 -0700

    Add wintersmith layout

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require("gulp");
 const pug = require("gulp-pug");
 
+const config = require('./config.json');
+
 const paths = {
   templates: {
     src: ["src/templates/**/*.pug", "!src/templates/layouts/**/*.pug"],
@@ -11,7 +13,7 @@ const paths = {
 function templates() {
   return gulp
     .src(paths.templates.src)
-    .pipe(pug())
+    .pipe(pug({ locals: config.locals }))
     .pipe(gulp.dest(paths.templates.dest));
 }
 
